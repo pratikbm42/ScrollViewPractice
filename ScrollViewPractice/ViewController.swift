068853080fc0fe1ad595203ad52a00bbfc2cb0e9
pratikbm44 <pratikmorge4444@gmail.com>
2023-10-07 16:30:18 +0530

    Initial Commit

---
+++ ScrollViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewPractice
+//
+//  Created by Mac on 07/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
